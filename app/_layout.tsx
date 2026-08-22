2205f28c259e691e77dc78e2be567418b29ac077
Vega Cinema Developer <dev@vegacinema.app>
2026-08-22 06:08:07 +0000

    fix: import missing InAppBrowserWarning component in app/_layout.tsx to fix white screen ReferenceError

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ import { DisclaimerModal } from '../components/DisclaimerModal';
 import { CommandSearch } from '../components/CommandSearch';
 import { ToastNotification } from '../components/ToastNotification';
 import { PWAInstallBanner } from '../components/PWAInstallBanner';
+import { InAppBrowserWarning } from '../components/InAppBrowserWarning';
 
 
 import { useAntiNewTab } from '../lib/antiNewTab';
